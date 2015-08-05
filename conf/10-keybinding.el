790d7097dcd27f82f280f4cbed14318701c4b968
SjB <steve@sagacity.ca>
2015-08-05 14:56:11 -0400

    fix: move all keybindings in keybinding.el

--- conf/10-keybinding.el
+++
@@ -1,6 +0,0 @@
-;; SjB keybinding file
-
-(global-set-key (kbd "C-'") 'other-window)
-(global-set-key (kbd "C-?") 'goto-line)
-
-(global-set-key (kbd "C-x g") 'magit-status)
---
+++ conf/keybinding.el
@@ -0,0 +1,87 @@
+;; SjB keybinding file
+
+(global-set-key (kbd "C-'") 'other-window)
+(global-set-key (kbd "C-?") 'goto-line)
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+
+(eval-after-load 'init-evil
+  '(progn
+     (evil-leader/set-key "w" 'save-buffer)
+     (evil-leader/set-key "q" 'kill-buffer-and-window)
+     (evil-leader/set-key "v" 'split-window-right)
+     (evil-leader/set-key "e" 'pp-eval-last-sexp)
+     (evil-leader/set-key "," 'other-window)
+     (evil-leader/set-key "b" 'helm-buffers-list)
+     (evil-leader/set-key "x" 'helm-M-x)
+
+     (evil-set-initial-state 'nav-mode 'emacs)
+     (evil-set-initial-state 'grep-mode 'emacs)
+
+     (eval-after-load 'dired-x
+       '(progn
+	  (evil-leader/set-key "h" 'dired-jump)
+	  ))
+
+     (eval-after-load 'dired
+       '(progn
+	  (defun my:dired-up-directory ()
+	    "Take dired up one directory, but behave like dired-find-alternate-file"
+	    (interactive)
+	    (let ((old (current-buffer)))
+	      (dired-up-directory)
+	      (kill-buffer old)
+	      ))
+
+	  (evil-define-key 'normal dired-mode-map "h" 'my:dired-up-directory)
+	  (evil-define-key 'normal dired-mode-map "l" 'dired-find-alternate-file)
+	  (evil-define-key 'normal dired-mode-map "o" 'dired-sort-toggle-or-edit)
+	  (evil-define-key 'normal dired-mode-map "v" 'dired-toggle-marks)
+	  (evil-define-key 'normal dired-mode-map "m" 'dired-mark)
+	  (evil-define-key 'normal dired-mode-map "u" 'dired-unmark)
+	  (evil-define-key 'normal dired-mode-map "U" 'dired-unmark-all-marks)
+	  (evil-define-key 'normal dired-mode-map "c" 'dired-create-directory)
+	  (evil-define-key 'normal dired-mode-map "n" 'evil-search-next)
+	  (evil-define-key 'normal dired-mode-map "N" 'evil-search-previous)
+	  (evil-define-key 'normal dired-mode-map "q" 'kill-this-buffer)
+	  ))
+
+     ;; Make HJKL keys work in special buffers
+     (eval-after-load 'magit-mode
+       '(progn
+	  (evil-set-initial-state 'magit-log-edit-mode 'emacs)
+	  (evil-add-hjkl-bindings magit-branch-manager-mode-map 'emacs
+	    "K" 'magit-discard-item
+	    "L" 'magit-key-mode-popup-logging)
+	  (evil-add-hjkl-bindings magit-status-mode-map 'emacs
+	    "K" 'magit-discard-item
+	    "l" 'magit-key-mode-popup-logging
+	    "h" 'magit-toggle-diff-refine-hunk)
+	  (evil-add-hjkl-bindings magit-log-mode-map 'emacs)
+	  (evil-add-hjkl-bindings magit-commit-mode-map 'emacs)
+	  ))
+
+     (eval-after-load 'occur-mode
+       '(progn
+	  (evil-add-hjkl-bindings occur-mode 'emacs)
+	  ))
+
+
+     (eval-after-load 'ibuffer
+       '(progn
+	  (evil-set-initial-state 'ibuffer-mode 'normal)
+	  (evil-define-key 'normal ibuffer-mode-map
+	    (kbd "m") 'ibuffer-mark-forward
+	    (kbd "t") 'ibuffer-toggle-forward
+	    (kbd "u") 'ibuffer-unmark-forward
+	    (kbd "=") 'ibuffer-diff-with-file
+	    (kbd "J") 'ibuffer-jump-to-buffer
+	    (kbd "M-g") 'ibuffer-jump-to-buffer
+	    (kbd "M-s a C-s") 'ibuffer-do-isearch
+	    (kbd "M-s a M-C-s") 'ibuffer-do-isearch-regexp
+	    (kbd "j") 'evil-next-line
+	    (kbd "k") 'evil-previous-line
+	    (kbd "l") 'ibuffer-visit-buffer
+	    (kbd "v") 'ibuffer-toggle-marks)
+	  ))
+     ))
