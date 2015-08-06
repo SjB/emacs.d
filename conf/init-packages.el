4dc8dc37069612bce32ce998b35d6f50b2ce7625
SjB <steve@sagacity.ca>
2015-08-06 16:43:27 -0400

    fix: configuring projectile

--- conf/init-packages.el
+++ conf/init-packages.el
@@ -8,10 +8,16 @@
 (yas-global-mode t)
 (setq yas-snippet-dirs (expand-file-name "snippets" user-emacs-directory))
 
+
 (require 'projectile)
-(require 'helm-projectile)
 (projectile-global-mode)
 (helm-projectile-on)
+(setq projectile-enable-caching t)
+(setq projectile-completion-system 'helm)
+(setq projectile-switch-project-action 'helm-projectile)
+;(setq projectile-keymap-prefix (kbd "M-u"))
+
+(require 'helm-projectile)
 
 (require 'magit)
 
--- keybinding.el
+++ keybinding.el
@@ -5,6 +5,7 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 (global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-p") 'projectile-command-map)
 
 (eval-after-load 'init-evil
   '(progn
