f0da410950f99c303e0f8992f9f0715eda477e5e
SjB <steve@sagacity.ca>
2015-08-07 08:31:28 -0400

    fix: cleaning up go environment config
    - move all require statement to top
    - added flycheck mode
    - rename env setting func

--- conf/init-go.el
+++ conf/init-go.el
@@ -1,11 +1,14 @@
 
 (require 'go-mode-autoloads)
 (require 'company-go)
+(require 'flycheck)
+(require 'projectile)
 
 (add-hook 'go-mode-hook (lambda ()
 			  ; use company for auto-complete
 			  (set (make-local-variable 'company-backends) '(company-go))
 			  (company-mode)
+			  (flycheck-mode)
 			  ; Use goimports instead of go-fmt
 			  (setq gofmt-command "goimports")
 			  ; Call Gofmt before saving
@@ -18,27 +21,25 @@
 			  (local-set-key (kbd "M-.") 'godef-jump)
 			  ))
 
-(require 'projectile)
-
-(defun go-env-setup (gopath)
+(defun generic-go-env-setting (gopath)
   (exec-path-from-shell-copy-env "PATH")
   (setenv "GOPATH" gopath)
   (setenv "GOBIN" (concat gopath "bin"))
   (message (concat "Project: " (getenv "GOPATH") " OS: " (getenv "GOOS") " ARCH: " (getenv "GOARCH")))
 )
 
-(defun go-env-linux64 ()
+(defun env-go-linux64 ()
   (interactive)
   (setenv "GOOS" "linux")
   (setenv "GOARCH" "amd64")
-  (go-env-setup (projectile-project-root))
+  (generic-go-env-settings (projectile-project-root))
 )
 
-(defun go-env-win64 ()
+(defun env-go-win64 ()
   (interactive)
   (setenv "GOOS" "windows")
   (setenv "GOARCH" "amd64")
-  (go-env-setup (projectile-project-root))
+  (generic-go-env-settings (projectile-project-root))
 )
 
 (provide 'init-go)
