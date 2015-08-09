75f449e5bcd6fd182677d1e57d7c5830cd93fccf
SjB <steve@sagacity.ca>
2015-08-09 12:17:29 -0400

    fix: keybinding for helm

--- keybinding.el
+++ keybinding.el
@@ -8,6 +8,16 @@
 (global-set-key (kbd "M-p") 'projectile-command-map)
 (global-set-key (kbd "C-c y") 'helm-yas-complete)
 
+; helm keybinding
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(define-key company-mode-map (kbd "C-:") 'helm-company)
+(define-key company-active-map (kbd "C-:") 'helm-company)
+
 (define-key evil-window-map "a" 'ace-window)
 
 (evil-leader/set-key "w" 'save-buffer)
