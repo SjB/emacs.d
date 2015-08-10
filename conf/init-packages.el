04674780c71765658ba0fc795daeb30f106f5cc6
SjB <steve@sagacity.ca>
2015-08-10 16:24:03 -0400

    fix: add yasnippet template to web-mode

--- conf/init-packages.el
+++ conf/init-packages.el
@@ -120,6 +120,10 @@
 (require 'yasnippet)
 (add-to-list 'yas-snippet-dirs (expand-file-name "snippets" user-emacs-directory))
 (add-to-list 'yas-snippet-dirs (expand-file-name "extra/yasnippet-go" user-emacs-directory))
+(add-hook 'web-mode-hook '(lambda ()
+			    (yas-activate-extra-mode 'html-mode)
+			    (yas-activate-extra-mode 'php-mode)
+			    (yas-activate-extra-mode 'css-mode)))
 (yas-global-mode t)
 
 (require 'company)
