fa4aaa237e66b18b2862ef4c5ada28d9f9698a4b
SjB <steve@sagacity.ca>
2015-08-10 02:33:17 -0400

    feature: added line and column number at bottom right

--- conf/init-packages.el
+++ conf/init-packages.el
@@ -113,6 +113,10 @@
  '(helm-gtags-ignore-case t)
  '(helm-gtags-auto-update t))
 
+(add-hook 'prog-mode-hook '(lambda ()
+			     (line-number-mode t)
+			     (column-number-mode t)
+			     ))
 (require 'yasnippet)
 (add-to-list 'yas-snippet-dirs (expand-file-name "snippets" user-emacs-directory))
 (add-to-list 'yas-snippet-dirs (expand-file-name "extra/yasnippet-go" user-emacs-directory))
