6eabe327442abaf84984dfd4bb97eec88bdfff65
SjB <steve@sagacity.ca>
2015-08-12 16:09:57 -0400

    fix: load projectile after all variable are set

--- modes.d/init-projectile-mode.el
+++ modes.d/init-projectile-mode.el
@@ -1,11 +1,13 @@
 (ensure-package-installed 'projectile)
 
 (require 'projectile)
-(projectile-global-mode)
+
 (helm-projectile-on)
 (setq projectile-enable-caching t)
 (setq projectile-completion-system 'helm)
 (setq projectile-switch-project-action 'helm-projectile)
 ;(setq projectile-keymap-prefix (kbd "M-u"))
+
+(projectile-global-mode t)
 
 (provide 'init-projectile-mode)
