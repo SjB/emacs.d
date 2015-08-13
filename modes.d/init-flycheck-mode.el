fd99de3a3c2d448db7dc579a18d6055a5979853b
SjB <steve@sagacity.ca>
2015-08-13 13:18:41 -0400

    feature: use web-mode by default with php files

--- modes.d/init-flycheck-mode.el
+++ modes.d/init-flycheck-mode.el
@@ -3,4 +3,18 @@
 (require 'flycheck)
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
+(flycheck-define-checker php-flycheck-reporter
+  "A PHP syntax checker using the PHP command line interpreter.
+
+See URL `http://php.net/manual/en/features.commandline.php'."
+  :command ("php" "-l" "-d" "error_reporting=E_ALL" "-d" "display_errors=1"
+	    "-d" "log_errors=0" source)
+  :error-patterns ((error line-start
+			  (or "Parse" "Fatal" "syntax") " error" (any ":" ",") " "
+			  (message) " in " (file-name) " on line " line line-end))
+  :modes (php-mode php+mode web-mode))
+
+(add-hook 'web-mode-hook (lambda ()
+						  (flycheck-select-checker 'php-flycheck-reporter)))
+
 (provide 'init-flycheck-mode)
--- modes.d/init-web-mode.el
+++ modes.d/init-web-mode.el
@@ -5,9 +5,10 @@
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.xml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.s?css\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.php\\.tpl\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.gohtml\\.gotmpl\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
 
 (setq web-mode-enable-auto-closing t)
 (setq web-mode-enable-auto-expanding t)
@@ -15,10 +16,14 @@
 (setq web-mode-enable-current-column-highlight t)
 (setq web-mode-enable-current-element-highlight t)
 
-(setq web-mode-ad-source-alist
-      '(("css" . (ac-source-css-property))
-	("html" . (ac-source-word-in-buffer ac-source-abbrev))
-	("php" . (ac-source-yasnippet ac-source-php-auto-yasnippets))
+(setq web-mode-ac-source-alist
+      '(("css" . (ac-source-words-in-buffer ac-source-css-property))
+	("html" . (ac-source-words-in-buffer ac-source-abbrev))
+	("php" . (ac-source-yasnippet
+		  ac-source-php-auto-yasnippets
+		  ac-source-words-in-buffer
+		  ac-source-words-in-same-mode-buffers
+		  ac-source-dictionary))
 	))
 
 (add-hook 'web-mode-hook '(lambda ()
