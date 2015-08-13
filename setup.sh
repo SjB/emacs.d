1957fba6bb22758e135a5639c836e5fcce80d30b
SjB <steve@sagacity.ca>
2015-08-12 23:05:56 -0400

    fix: set all variable then change to company-mode

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ mkdir -p $EMACSDIR/snippets
 
 sudo apt-get install build-essential clang exuberant-ctags editorconfig libncurses5-dev
 
-wget https://raw.githubusercontent.com/dominikh/go-errcheck.el/master/go-errcheck.el -O $EMACSDIR/conf/go-errcheck.el
+#wget https://raw.githubusercontent.com/dominikh/go-errcheck.el/master/go-errcheck.el -O $EMACSDIR/conf/go-errcheck.el
 #wget https://raw.githubusercontent.com/nsf/gocode/master/emacs/go-autocomplete.el -O $EMACSDIR/conf/go-autocomplete.el
-wget https://raw.githubusercontent.com/golang/tools/master/cmd/oracle/oracle.el -O $EMACSDIR/conf/oracle.el
+#wget https://raw.githubusercontent.com/golang/tools/master/cmd/oracle/oracle.el -O $EMACSDIR/conf/oracle.el
 wget https://raw.githubusercontent.com/google/styleguide/gh-pages/cpplint/cpplint.py -O $EMACSDIR/bin/cpplint.py
 chmod a+x $EMACSDIR/bin/cpplint.py
 
