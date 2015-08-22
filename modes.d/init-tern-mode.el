c813af9502e3333981c0dfeb9e823c64b00520be
SjB <steve@sagacity.ca>
2015-08-21 22:24:21 -0400

    fix: load tern when in js3-mode

--- modes.d/init-tern-mode.el
+++ modes.d/init-tern-mode.el
@@ -2,10 +2,13 @@
  'tern
  'tern-auto-complete)
 
-(add-hook 'js-mode-hook (lambda () (tern-mode t)))
+(eval-after-load 'js3-mode
+  (add-hook 'js3-mode-hook '(lambda() (tern-mode t))))
+
 
 (eval-after-load 'tern
   '(progn
+     (setq tern-command (cons (executable-find "tern") '()))
      (require 'tern-auto-complete)
      (tern-ac-setup)))
 
