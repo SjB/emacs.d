554ca9f610256937e89e9f325a2dc4d8d1639a31
SjB <steve@sagacity.ca>
2015-08-21 22:21:33 -0400

    fix: jsx-mode is a major-mode

--- modes.d/init-jsx-mode.el
+++ modes.d/init-jsx-mode.el
@@ -1,5 +1,16 @@
 (ensure-package-installed 'jsx-mode)
 
-(add-hook 'jsx-mode-hook 'js2-minor-mode)
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . jsx-mode))
+
+(eval-after-load 'jsx-mode
+  '(setq
+    jsx-use-auto-complete t
+    jsx-syntax-check-mode "compile"
+    jsx-use-flycheck nil)
+
+;  (define-key jsx-mode-map (kbd "RET") 'newline-and-indent)
+;  (define-key jsx-mode-map (kbd "C-c TAB") 'jsx-auto-complete)
+  )
+
 
 (provide 'init-jsx-mode)
